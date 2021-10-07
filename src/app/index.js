e8b3ec7c698e6853658dae3a8dd4efcc54b7512f
DavidSC <david@wbcboxing.com>
2021-10-07 03:14:33 -0500

    agregadno mas a la ui

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,7 @@
 const { Weather } = require('./Weather');
+const { UI } = require('./UI');
+
+const ui = new UI();
 const weather = new Weather('London', 'uk');
 
 require('./index.css');
@@ -6,6 +9,7 @@ require('./index.css');
 async function fetchweather() {
     const data = await weather.getWeather();
     console.log(data);
+    ui.render(data);
 }
 
 document.addEventListener('DOMContentLoaded', fetchweather);
